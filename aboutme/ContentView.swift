2556d3d36177984e91483189f7faa8cbcdc4dcd5
MiaBella Gonzalez <miabellaglzglz@gmail.com>
2023-07-17 09:53:13 -0500

    Initial Commit

---
+++ aboutme/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  aboutme
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//
+//            Image("Madam Marie Curie").resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 500.0, height: 500.0)
+//                .cornerRadius(5)
+            
+            
+            Text("MiaBella Gonzalez")
+                .font(.title)
+            
+            Text("I'm 14 years old so an incoming freshman.So far the only careers that intreist me are criminal justice, forensic science,and liberal arts.")
+        
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
